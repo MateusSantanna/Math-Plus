48edcf6660bfc1a2216f582407dd88f5fc59d9c9
MateusSantanna <mateussantanna2001@outlook.com>
2023-07-25 11:43:08 -0300

    Error time Fixed

--- src/components/CounterQuestions/CounterQuestions.jsx
+++ src/components/CounterQuestions/CounterQuestions.jsx
@@ -1,3 +1,43 @@
+// import { CounterTimeQuestion, TimeCritical } from "./style";
+
+// function CounterQuestions({
+//   counterQuestions,
+//   setCounterQuestions,
+//   chances,
+//   setChances,
+//   resultsGame,
+//   setResultsGame,
+// }) {
+//   function changeTimeGame() {
+//     if (counterQuestions > 0) {
+//       setCounterQuestions(counterQuestions - 1);
+//     }
+
+//     if (counterQuestions === 0) {
+//       setCounterQuestions(0);
+//       setResultsGame("Tempo Esgotado");
+//       setChances(chances - 1);
+//     }
+//   }
+
+//   resultsGame === "Resposta Correta" || resultsGame === "Resposta Errada"
+//     ? setCounterQuestions(10)
+//     : setTimeout(changeTimeGame, 1000);
+
+//   return counterQuestions > 4 ? (
+//     <>
+//       <CounterTimeQuestion>{counterQuestions}</CounterTimeQuestion>
+//     </>
+//   ) : (
+//     <>
+//       <TimeCritical>{counterQuestions} </TimeCritical>
+//     </>
+//   );
+// }
+
+// export default CounterQuestions;
+
+import { useState, useEffect } from "react";
 import { CounterTimeQuestion, TimeCritical } from "./style";
 
 function CounterQuestions({
@@ -8,29 +48,37 @@ function CounterQuestions({
   resultsGame,
   setResultsGame,
 }) {
-  function changeTimeGame() {
-    if (counterQuestions > 0) {
-      setCounterQuestions(counterQuestions - 1);
+  const [timer, setTimer] = useState(10);
+
+  useEffect(() => {
+    let interval;
+    if (counterQuestions > 0 && timer > 0) {
+      interval = setInterval(() => {
+        setTimer((prevTimer) => prevTimer - 1);
+      }, 1000);
     }
 
-    if (counterQuestions === 0) {
-      setCounterQuestions(0);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [counterQuestions, timer]);
+
+  useEffect(() => {
+    if (timer === 0) {
       setResultsGame("Tempo Esgotado");
-      setChances(chances - 1);
-    }
-  }
+      setChances((prevChances) => prevChances - 1);
 
-  resultsGame === "Resposta Correta" || resultsGame === "Resposta Errada"
-    ? setCounterQuestions(10)
-    : setTimeout(changeTimeGame, 1000);
+      setTimer(10);
+    }
+  }, [timer, setResultsGame, setChances]);
 
   return counterQuestions > 4 ? (
     <>
-      <CounterTimeQuestion>{counterQuestions}</CounterTimeQuestion>
+      <CounterTimeQuestion>{timer}</CounterTimeQuestion>
     </>
   ) : (
     <>
-      <TimeCritical>{counterQuestions} </TimeCritical>
+      <TimeCritical>{resultsGame}</TimeCritical>
     </>
   );
 }
